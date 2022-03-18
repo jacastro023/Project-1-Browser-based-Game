b1da2cd852ea728bd291f7e4bb1824fc22ea20ed
Jose Castro <jacastro023@gmail.com>
2022-03-18 13:13:38 -0500

    created variables and grabbed user choice and passed it though if else statements to create different size grids and update various variables

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let flagsLeft = document.querySelector(".flagsLeft");
 let totalSeconds = 0; //variable to hold the seconds
 let rows = "" 
 let columns = ""
+let boardButtons = ""
+let numsArray = ""
 
 
 // setting timer to display seconds and minutes
@@ -30,6 +32,70 @@ function timer(val)
         return valString; // else just return the value of seconds
     }
 }
+
+// getting the selected choice from the user
+function inputValue() {
+    let radios = document.getElementsByName('choice');
+    let val= "";
+    for (i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+// console.log(val)
+friends = val
+// console.log(friends)
+    if(friends === ""){
+        alert("Please make a choice")
+    } else if(friends === "5"){
+        startingDiv.style.display = "none";
+        buildGrid(4, 4);
+        status.style.display = "flex";
+        friendsLeft.innerText = "5";
+        flagsLeft.innerText = "10";
+        setInterval(setTime, 1000);
+        randomUniqueNumbers(10, 5);
+        rows = 4;
+        columns = 4;
+        friends = 5;
+        console.log(nums)
+        boardButtons = document.querySelectorAll(".gameBoard > button");
+        // console.log(boardButtons)
+        addFriends();
+    } else if(friends === "10"){
+        startingDiv.style.display = "none";
+        buildGrid(9, 9);
+        status.style.display = "flex";
+        friendsLeft.innerText = "10";
+        flagsLeft.innerText = "20";
+        setInterval(setTime, 1000);
+        randomUniqueNumbers(81, 10);
+        friends = 10;
+        rows = 9;
+        columns = 9;
+        // console.log(nums)
+        boardButtons = document.querySelectorAll(".gameBoard > button");
+        // console.log(boardButtons)
+        addFriends();
+    } else if(friends === "30"){
+        startingDiv.style.display = "none";
+        buildGrid(12, 12);
+        status.style.display = "flex";
+        friendsLeft.innerText = "30";
+        flagsLeft.innerText = "50";
+        setInterval(setTime, 1000);
+        randomUniqueNumbers(144, 30);
+        friends = 30;
+        rows = 12;
+        columns = 12;
+        // console.log(nums)
+        boardButtons = document.querySelectorAll(".gameBoard > button");
+        // console.log(boardButtons)
+        addFriends();
+    }
+
+  };
 
 const gameContainer = document.querySelector(".gameBoard");
 //   console.log(gameContainer)
